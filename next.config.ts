32e088b5a2c599beacc740e14668bb32cc21769a
minerva-jupiter <ryouturn@gmail.com>
2025-09-27 15:28:33 +0900

    delete nextconfig setting

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,6 @@
 import type { NextConfig } from 'next';
 
-const repositoryName: string = 'nextjs';
-const isProduction: boolean = process.env.NODE_ENV === 'production';
-
 const nextConfig: NextConfig = {
-  devIndicators: false,
-  output: 'export', // 静的書き出し
-  ...(isProduction // 本番環境ではbasePath、assetPrefixを変更
-    ? {
-        basePath: `/${repositoryName}`,
-        assetPrefix: `/${repositoryName}/`,
-      }
-    : {}),
 };
 
 export default nextConfig;
